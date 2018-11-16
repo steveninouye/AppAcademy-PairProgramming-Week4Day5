4cb7191c8c8845090997c765b2e79e824aed7197
Steven Inouye <steveninouye@msn.com>
2018-11-16 11:56:21 -0800

    Add test for User #reset_session_token

--- resolutions/spec/models/user_spec.rb
+++ resolutions/spec/models/user_spec.rb
@@ -15,4 +15,24 @@ RSpec.describe User, type: :model do
       expect(user.session_token).not_to be_nil
     end
   end
+  
+  describe '#reset_session_token' do
+    
+    it 'user should have a different session token' do
+      good_user.session_token = 'banana'
+      good_user.reset_session_token
+      expect(good_user.session_token).to_not eq('banana')
+    end
+    
+    it 'returns a session token' do
+      token = good_user.reset_session_token
+      expect(good_user.session_token).to eq(token)
+    end  
+  end
+  
+  
+  
+  # password=(password)
+  # is_password?(password)
+  # ::validate_user_credentials(username, password)
 end
